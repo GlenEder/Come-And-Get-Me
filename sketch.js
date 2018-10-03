12f40fc7d173f7ae57cad5a0edf2d76f96c36621
Glen Michael Austin Eder <geder@purdue.edu>
2018-10-03 13:06:12 -0400

    Adds enemy object

--- sketch.js
+++ sketch.js
@@ -3,6 +3,10 @@ let canvasHeight = 500;
 
 
 let player = new Player();
+let enimes = [];
+enimes[0] = new Enemy(100, 100, "red");
+enimes[1] = new Enemy(canvasWidth / 2, 20, "green");
+enimes[2] = new Enemy(canvasWidth - 40, canvasHeight - 60, "blue");
 
 function setup() {
     createCanvas(canvasWidth, canvasHeight);
@@ -17,17 +21,62 @@ function draw() {
 
     //update logic
     player.update();
+    for(i = 0; i < enimes.length; i++) {
+        enimes[i].update();
+    }
 
     //update graphics
     player.render();
+    for(i = 0; i < enimes.length; i++) {
+        enimes[i].render();
+    }
 
+}
+
+function Enemy(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.xSpeed = 2;
+    this.ySpeed = 2;
+    this.adjustRate = 1;
+    this.radius = 5;
+    this.color = color;
+
+    this.update = function() {
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+
+        
+        //check bounds
+        if(this.x - this.radius < 0) {
+            this.x = this.radius;
+            this.xSpeed *= -1;
+        }else if(this.x + this.radius > canvasWidth) {
+            this.x = canvasWidth - this.radius;
+            this.xSpeed *= -1;
+        }
+
+        if(this.y - this.radius < 0) {
+            this.y = this.radius;
+            this.ySpeed *= -1;
+        }else if(this.y + this.radius > canvasHeight) {
+            this.y = canvasHeight - this.radius;
+            this.ySpeed *= -1;
+        }
+    }
+
+    this.render = function() {
+        ellipseMode(RADIUS);
+        fill(color);
+        ellipse(this.x, this.y, this.radius);
+    }
 }
 
 function Player() {
     this.x = canvasWidth / 2;
     this.y = canvasHeight / 2;
     this.speed = 3;
-    this.radius = 5;
+    this.radius = 7;
 
     this.update = function() {
 
